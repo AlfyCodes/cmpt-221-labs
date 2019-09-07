4e06180880a3fcb36ef8fafb2a322ff232d192a7
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-07 19:21:49 -0400

    Add *100 to get rid of decimal and test quarters

--- lab1/main.py
+++ lab1/main.py
@@ -7,8 +7,18 @@ money_gave = eval(input("How much money did you give?"))
 #Compute the change
 change_back = money_gave - items_cost
 
+#Get rid of the decimal
+chancge_back = change_back * 100
+
 #Declarations of Coins
-quarter = 25
-dime = 10
-nickel = 5
-penny = 1
+quarter = .25
+dime = .10
+nickel = .5
+penny = .01
+
+#Find the amount of Quarters we need
+amount_of_quarters = int(change_back // quarter)
+
+
+
+print('Quarters' +str(amount_of_quarters))
