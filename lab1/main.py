8cafd75fd5e89b50980c70bfdd26a86e17cc9552
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-07 12:09:16 -0400

    Computed change_back variable and printed to test

--- lab1/main.py
+++ lab1/main.py
@@ -1,2 +1,10 @@
 #Find the total cost of the customer's items
 items_cost = eval(input("What is the total cost of your items?"))
+
+#Ask user how much money was given
+money_gave = eval(input("How much money did you give?"))
+
+#Compute the change
+change_back = money_gave - items_cost
+
+print(change_back)
