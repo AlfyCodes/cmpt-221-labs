637d2fa47e6ec64aa97587b0e6a07ad3c06ed54b
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-08 08:01:48 -0400

    Created Parent class Vechicle and Child Motorcycle

---
+++ lab1/Vehicle Classes & Objects.py
@@ -0,0 +1,8 @@
+class Vechicle:
+    def __init__(self, make,model,color)
+        self.make = make
+        self.model = model 
+        self.color = color
+        
+class Motocycle(Vechicle):
+    pass        
