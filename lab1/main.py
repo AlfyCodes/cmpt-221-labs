2c34ead065ec6d60629955fb648416c622dc46a4
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-07 20:02:49 -0400

    Added Dimes and tested with print

--- lab1/main.py
+++ lab1/main.py
@@ -8,17 +8,23 @@ money_gave = eval(input("How much money did you give?"))
 change_back = money_gave - items_cost
 
 #Get rid of the decimal
-chancge_back = change_back * 100
+change_back = change_back * 100
 
 #Declarations of Coins
-quarter = .25
-dime = .10
-nickel = .5
-penny = .01
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1
 
 #Find the amount of Quarters we need
 amount_of_quarters = int(change_back // quarter)
 
+print('Quarters: ' +str(amount_of_quarters))
 
+#Update change_back 
+change_back = change_back - (amount_of_quarters * quarter)
 
-print('Quarters' +str(amount_of_quarters))
+#Find the amount of Dimes to return
+amount_of_dimes = int(change_back // dime)
+
+print('Dimes: ' +str(amount_of_dimes))
