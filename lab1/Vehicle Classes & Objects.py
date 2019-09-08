de206b74a4d5fe5abf756a428797d73b2be758f7
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-08 08:43:58 -0400

    Changed location of properties, started drive func

--- lab1/Vehicle Classes & Objects.py
+++ lab1/Vehicle Classes & Objects.py
@@ -1,16 +1,26 @@
 class Vechicle:
-    def __init__(self, make,model,color,fuel_max,fuel_current,miles_current):
+    def __init__(self, make,model,color):
         self.make = make
         self.model = model 
         self.color = color
+       
+    def drive(self,fuel_current,miles_current):
+        pass
+class Motorcycle(Vechicle):
+    def __init__(self,fuel_max,fuel_current,miles_current):
+        self.fuel_max = fuel_max
+        self.fuel_current = fuel_current
+        self.miles_current = miles_current        
+class Truck(Vechicle):
+    def __init__(self,fuel_max,fuel_current,miles_current):
+        self.fuel_max = fuel_max
+        self.fuel_current = fuel_current
+        self.miles_current = miles_current    
+class Bus(Vechicle):
+    def __init__(self,fuel_max,fuel_current,miles_current):
         self.fuel_max = fuel_max
         self.fuel_current = fuel_current
         self.miles_current = miles_current
-class Motorcycle(Vechicle):
-    pass        
-class Truck(Vechicle):
-    pass
 class Bicycle(Vechicle):
-    pass
-class Bus(Vechicle):
-    pass
+    def __init__(self,condition):
+        self.condition = condition
