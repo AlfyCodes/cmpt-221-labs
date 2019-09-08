5610a22e6f6d8feb02bd980f3f56d5322b1a1789
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-08 08:21:59 -0400

    Added new properties to Vechicle class

--- lab1/Vehicle Classes & Objects.py
+++ lab1/Vehicle Classes & Objects.py
@@ -1,8 +1,11 @@
 class Vechicle:
-    def __init__(self, make,model,color):
+    def __init__(self, make,model,color,fuel_max,fuel_current,miles_current):
         self.make = make
         self.model = model 
         self.color = color
+        self.fuel_max = fuel_max
+        self.fuel_current = fuel_current
+        self.miles_current = miles_current
 class Motorcycle(Vechicle):
     pass        
 class Truck(Vechicle):
