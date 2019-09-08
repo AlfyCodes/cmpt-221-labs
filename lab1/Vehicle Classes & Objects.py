521701ebe7c90ddd9aef9e17f76faeb8c8c824d4
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-08 08:15:20 -0400

    Created more child classes / clean up spelling and neatness

--- lab1/Vehicle Classes & Objects.py
+++ lab1/Vehicle Classes & Objects.py
@@ -1,8 +1,13 @@
 class Vechicle:
-    def __init__(self, make,model,color)
+    def __init__(self, make,model,color):
         self.make = make
         self.model = model 
         self.color = color
-        
-class Motocycle(Vechicle):
+class Motorcycle(Vechicle):
     pass        
+class Truck(Vechicle):
+    pass
+class Bicycle(Vechicle):
+    pass
+class Bus(Vechicle):
+    pass
