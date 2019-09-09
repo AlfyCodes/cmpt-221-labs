bd8f0bd07fe7a9125b84ca80a4de59c0f5be6dc4
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-08 22:35:11 -0400

    change varible names / added to main func

--- lab1/Vehicle Classes & Objects.py
+++ lab1/Vehicle Classes & Objects.py
@@ -3,21 +3,21 @@ Parent Class Vehicle
 will construct properties for vechicle type
 '''
 class Vehicle:
-    def __init__(self,make,model,color,fuel_max,fuel_current,miles_current):
+    def __init__(self,make,model,color,gas_capacity,gas_milage,milage):
         self.make = make
         self.model = model 
         self.color = color
-        self.fuel_max = fuel_max
-        self.fuel_current = fuel_current
-        self.miles_current = miles_current
+        self.gas_capacity = gas_capacity
+        self.gas_milage = gas_milage
+        self.milage = milage
        
     def print_data(self):
 
-        print(self.make,self.model,self.color,self.fuel_max,self.fuel_current,self.miles_current)
+        print(self.make,self.model,self.color,self.gas_capacity,self.gas_milage,self.milage)
 
-    def drive(self,fuel_current,miles_current,total_drive):
-        self.miles_current = self.miles_current + total_drive
-        self.fuel_current = fuel_current - total_drive
+    def drive(self,gas_milage,milage,total_drive):
+        self.milage = self.milage + total_drive
+        self.gas_milage = gas_milage - total_drive
     
 '''
 Child Class Motorcycle
@@ -25,51 +25,71 @@ Will handle three properties
 CURRENTLY TESTING THIS CLASS ONLY
 '''
 class Motorcycle(Vehicle):
-    def __init__(self,make,model,color,fuel_max,fuel_current,miles_current):
-        super().__init__(make,model,color,fuel_max,fuel_current,miles_current)
+    def __init__(self,make,model,color,gas_capacity,gas_milage,milage):
+        super().__init__(make,model,color,gas_capacity,gas_milage,milage)
 '''
 Child Class Truck
 Will handle three properties
 '''        
 class Truck(Vehicle):
-    def __init__(self,make,model,color,fuel_max,fuel_current,miles_current):
-        super().__init__(make,model,color,fuel_max,fuel_current,miles_current)
+    def __init__(self,make,model,color,gas_capacity,gas_milage,milage):
+        super().__init__(make,model,color,gas_capacity,gas_milage,milage)
         
 '''
 Child Class Bus
 Will handle three properties
 '''    
 class Bus(Vehicle):
-    def __init__(self,make,model,color,fuel_max,fuel_current,miles_current):
-        super().__init__(make,model,color,fuel_max,fuel_current,miles_current)
+    def __init__(self,make,model,color,gas_capacity,gas_milage,milage):
+        super().__init__(make,model,color,gas_capacity,gas_milage,milage)
 '''
 Child Class Bicycle
 Will handle it's own property condition
 '''
 class Car(Vehicle):
-    def __init__(self,make,model,color,fuel_max,fuel_current,miles_current):
-        super().__init__(make,model,color,fuel_max,fuel_current,miles_current)
+    def __init__(self,make,model,color,gas_capacity,gas_milage,milage):
+        super().__init__(make,model,color,gas_capacity,gas_milage,milage)
 
-class main():
+'''
+Main function to handle all user based inputs
+'''
     
+def main():
+
     def get_data()
+
+        print ('What type of Vehicle do you have? [Car/Truck/Motorcycle/Bus]')
+        type = input()
+
+        if type != 'Car' or type != 'Truck' or type != 'Motorcycle' or type != 'Bus':
+            print('Please enter a valid Vehicle type.')
+            main()
+
+
         print ('What is the Make of your vehicle?')
         make = input()
+        
+        print('What is the Model of your vehicle?')
+        model = input()
 
-    print('What is the Model of your vehicle?')
-    model = input()
+        print('What is the Color of your vehicle?')
+        color = input()
 
-    print('What is the Color of your vehicle?')
-    color = input()
+        print('What is the max Gas Miles of your vehicle?')
+        gas_capacity = input()
+
+        print('What is the current Gas Miles of your vehicle?')
+        gas_milage = input()
+
+        print('What is the Milage of your vehicle?')
+        milage = input()
+        
+        return(make,model,color,gas_capacity,gas_milage,milage)
 
 '''
 Asking the User the questions to store data
 '''
 
-
-bike = Motorcycle(make,model,color,20,10,1000)
-
-bike.print_data()
  
 #Vehicle.drive(bike)
 
