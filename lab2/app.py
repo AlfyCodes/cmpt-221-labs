91e171ff1c3fe78d901c0a689d103c2b5e977189
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-22 19:02:59 -0400

    added about me page, adding list. getting use to pycharm overall

--- lab2/app.py
+++ lab2/app.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def view_index():
-    return "<h1> Hey there! </h1> <p> Can you do me a favor and write your name at the end of the url Like this: '/user/yourname'?</p> <p> Or if you want go check out the Welcome Page here:</p>"
+    return "<h1> Welcome! </h1> <p> You can tell me your name by: '/user/yourname'</p> <p> You can visit the About " \
+           "Me Page here: <a href='http://127.0.0.1:5000/about'>About Page</a> </p>"
+
 
 @app.route("/user/<username>")
 def user_name(username):
-    return "<h1> Hey " +username+ "!</h1>"
+    return "<h1> Hey " + username + "!</h1>"
 
-@app.route("/welcome")
-def welcome_page():
-    return "<h1> Welcome to the most awesomest page "
+
+@app.route("/about")
+def about_page():
+    return "<h1> Welcome to the most awesomest page! <p> Why is this the most awesomest page?!? </p> <ul> "
 
 if __name__ == "__main__":
     app.run()
