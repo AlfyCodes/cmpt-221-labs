8a0eaad2435e783396e70577c6e7189da8f5eafe
MrNomNomsTheVI <54919379+mrnomnomsthevi@users.noreply.github.com>
2019-09-22 18:43:23 -0400

    switched to pycharm. added pages: index, username, welcome

--- lab2/app.py
+++ lab2/app.py
@@ -4,9 +4,17 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/index")
 def view_index():
-    return "<h1> hi </h1>"
+    return "<h1> Hey there! </h1> <p> Can you do me a favor and write your name at the end of the url Like this: '/user/yourname'?</p> <p> Or if you want go check out the Welcome Page here:</p>"
 
+@app.route("/user/<username>")
+def user_name(username):
+    return "<h1> Hey " +username+ "!</h1>"
+
+@app.route("/welcome")
+def welcome_page():
+    return "<h1> Welcome to the most awesomest page "
 
 if __name__ == "__main__":
     app.run()
