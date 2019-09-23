80a3ab55fe80f54cb73a47b4b5134b0c6516d057
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-23 00:48:38 -0400

    fixed few things in css style, added static folder to structure flask. Started resume.html

--- lab2/Resume Project/theframe.py
+++ lab2/Resume Project/theframe.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "<h1> Welcome! </h1>"\
-            "<p> My name is Alfred Ortiz Jr. I grew up in the Bronx for 16 years then moved to Walden,Ny. I've lived " \
-           "in Middletown, Newburgh, and now reside in Montgomery. I'm 29 years old with a wife and a son. "
+    return render_template("index.html")
 
 
 
