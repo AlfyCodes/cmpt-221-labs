da495aa146afca08fcd841d96147ea2c6a790a41
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-22 20:21:47 -0400

    added some details to my welcome page

--- lab2/Resume.py
+++ lab2/Resume.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-    return "<h1> Welcome! </h1>"
+    return "<h1> Welcome! </h1>"\
+            "<p> My name is Alfred Ortiz Jr. I grew up in the Bronx for 16 years then moved to Walden,Ny. I've lived " \
+           "in Middletown, Newburgh, and now reside in Montgomery. I'm 29 years old with a wife and a son. "
+
+
 
 if __name__ == "__main__":
     app.run()
