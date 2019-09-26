9902796b19796a396d146b15476d6e678fd1bc7c
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-09-26 13:49:52 -0400

    Reconstruct app.py to seperate htmls  and render

--- lab2/WelcomePageProject/app.py
+++ lab2/WelcomePageProject/app.py
@@ -4,14 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/resume")
-def resume():
-    return render_template("resume.html")
-
+def view_index():
+    return render_template("index.html")           
 
 if __name__ == "__main__":
     app.run()
