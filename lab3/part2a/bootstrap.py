21578b66dee36f094aa55dca83e74f299ed40226
MrNomNomsTheVI <54919379+MrNomNomsTheVI@users.noreply.github.com>
2019-10-07 00:25:02 -0400

    add functionality to handle forms

--- lab3/part2a/bootstrap.py
+++ lab3/part2a/bootstrap.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template;
+from flask import Flask, flash, print, redirect, render_template;
+#import from forms.py that was created
+from InClass.FlaskWTF.forms import ContactForm
 
 #Flask Object
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "Teacher Leave Them Kids Alone"
+
 #Set routes for index.html
 @app.route('/')
-@app.route('/root')
 def root():
+
+    forms = ContactForm()
+
+    #Check for submittals
+
+    if forms.validateOnSubmit():
+        print("ok")
+        flash("A new form has been submitted: Name {}, Phone {}, Message{}" .format(forms.name.data, forms.email.data, forms.phone_number.data, forms.message.data))
     #return index html
     return render_template('index.html', name='Alfred Ortiz Jr', workType='Engineer - National Guard Air Force - Father'), 200
 
